b06e34f2621edd7e3ab83157268e09a670c99a4b
Tanki <tanki_jong@hotmail.com>
2015-11-08 06:34:33 +0800

    player actions

--- src/GameScene.js
+++ src/GameScene.js
@@ -4,9 +4,11 @@ var GameScene = cc.Scene.extend({
     ufo:null,
     user_1:null,
     user_2:null,
-
+    phisicalEngine:null,
     gameLayer:null,
-
+    me:null,
+    other:null,
+    spacecraft:null,
     ctor:function(){
         this._super();
         this.init();
@@ -15,6 +17,7 @@ var GameScene = cc.Scene.extend({
     init: function(){
         this._super();
         var gameLayer = ccs.load(res.GameLayer_json).node;
+        var me,other;
         this.gameLayer =gameLayer;
         cc.log('success');
         this.addChild(gameLayer);
@@ -27,14 +30,54 @@ var GameScene = cc.Scene.extend({
             cc.log(this[items[i]]?'getttttt':'Nooooooo');
         }
 
+        var self= this;
         var updateStatusListener = cc.EventListener.create({
             event: cc.EventListener.CUSTOM,
             eventName: "update_status",
             callback: function(event){
-                cc.log("update called");
+                var data=event.getData();
+                me.changeSpeed(data.mySpeed);
+                other.changeSpeed(data.hisSpeed);
+                self.updatePlanetRotation(data.planetSpeed);
             }
         });
         cc.eventManager.addListener(updateStatusListener, 1);
+
+        //this.phisicalEngine = new PhisicalEngine();
+
+        //var PlayerAnimationCtrl = function (animationStr) {
+        //    var startRunAnimation = ccs.load(animationStr).action;
+        //    startRunAnimation.gotoFrameAndPlay(0,90,true);
+        //    var stopRunAnimation = ccs.load(animationStr).action;
+        //    stopRunAnimation.gotoFrameAndPlay(0,90,false);
+        //    var byeAnimation = ccs.load(animationStr).action;
+        //    byeAnimation.gotoFrameAndPlay(0,90,true);
+        //    this.startRun =function(){
+        //        gameLayer.stopActionByTag(stopRunAnimation.tag);
+        //        gameLayer.stopActionByTag(byeAnimation.tag);
+        //        startRunAnimation.setTimeSpeed(1);
+        //        gameLayer.runAction(startRunAnimation);
+        //    };
+        //
+        //    this.changeSpeed =function(spd){
+        //        startRunAnimation.setTimeSpeed(UnitConversion(spd));
+        //    }
+        //
+        //    this.stopRun = function(){
+        //        gameLayer.stopActionByTag(byeAnimation.tag);
+        //        gameLayer.stopActionByTag(startRunAnimation.tag);
+        //        startRunAnimation.setTimeSpeed(1);
+        //        gameLayer.runAction(stopRunAnimation);
+        //    };
+        //    this.bye = function(){
+        //        gameLayer.stopActionByTag(stopRunAnimation.tag);
+        //        gameLayer.stopActionByTag(startRunAnimation.tag);
+        //        gameLayer.runAction(byeAnimation);
+        //    }
+        //}
+        //
+        //this.me = me = new PlayerAnimationCtrl(res.Me_animation);
+        //this.other = other = new PlayerAnimationCtrl(res.Him_animation);
     },
 
     onEnter:function(){
@@ -51,13 +94,17 @@ var GameScene = cc.Scene.extend({
             action = ccs.load(res.Planet_animation).action;
             cc.log(action?'getttttt':'Nooooooo');
             action.gotoFrameAndPlay(0,90,true);
+            action.setTimeSpeed(UnitConversion(1));
             action = this.gameLayer.runAction(action);
             this.scheduleUpdate();
             this.planet._action =action;
+            //GameScene.ac=action;
         }
         else {
-            action = this.this.planet._action;
-            action.setDelayPerUnit(UnitConversion(speed));
+            action = this.planet._action;
+            var f= action.getCurrentFrame();
+            action.setTimeSpeed(UnitConversion(speed));
+            action.setCurrentFrame(f);
         }
     },
 
@@ -75,7 +122,9 @@ var GameScene = cc.Scene.extend({
         cc.schedule(this.tik, 0.1);
     },
     update:function(){
-        if(this.planet._action.isPlaying())
+        if(this.planet._action.isPlaying()){
             cc.log('isPlaying');
+            this.count++;
+        }
     }
 });
--- src/resource.js
+++ src/resource.js
@@ -2,7 +2,9 @@ var res = {
     HelloWorld_png : "res/HelloWorld.png",
     MainScene_json : "res/MainScene.json",
     GameLayer_json : "res/generalPlanet.json",
-    Planet_animation : "res/planet.json"
+    Planet_animation : "res/planet.json",
+    Me_animation : "res/user_1.json",
+    Him_animation : "res/user_2.json",
 };
 
 var g_resources = [];
